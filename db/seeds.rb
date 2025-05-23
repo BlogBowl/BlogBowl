18520855c102841a533fd76af7c124ddb62bd076
Oleksandr Honcharov <oleksandr.honcharov@windy.com>
2025-05-23 14:55:44 +0200

    feat: model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# FOR OSS
+
+ActiveRecord::Base.transaction do
+  user = User.create!(email: "admin@example.com", password: "changeme")
+
+  workspace = Workspace.new(title: "My Workspace")
+  user.members.create!(workspace:, permissions: ["owner"])
+  author = user.members.first.create_or_activate_author!
+
+  first_page = workspace.pages.first
+  unless first_page.nil?
+    first_page.create_default_first_post(author.id)
+  end
+
+end
