16cd984fbb57e8564ca01c40a9aa0bc17f7e852e
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-07 17:13:55 +0200

    chore: add sidekiq and redis back

--- config/initializers/session_store.rb
+++
@@ -1,7 +0,0 @@
-cookie_domain = ['', 'analytics.'].map do |subdomain|
-  subdomain + Rails.application.routes.default_url_options[:host]
-end
-
-Rails.application.config.session_store :cookie_store,
-                                       key: '_blogbowl_session',
-                                       domain: cookie_domain
