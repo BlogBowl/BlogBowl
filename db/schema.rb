00d5e83fc0118a87c92d2396c3dd757bc2f0c721
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-13 13:19:23 +0200

    feat: update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_06_140320) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_184008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -310,6 +310,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_06_140320) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.jsonb "dismissed_notices", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,7 @@
 # FOR OSS
 
 ActiveRecord::Base.transaction do
-  user = User.create!(email: "admin@example.com", password: "changeme")
-
-  workspace = Workspace.new(title: "My Workspace")
-  user.members.create!(workspace:, permissions: ["owner"])
-  author = user.members.first.create_or_activate_author!
-
-  first_page = workspace.pages.first
-  unless first_page.nil?
-    first_page.create_default_first_post(author.id)
-  end
-
+  puts "Creating default user..."
+  User.create!(email: "admin@example.com", password: "changeme")
+  puts "Default user admin@example.com was created"
 end
