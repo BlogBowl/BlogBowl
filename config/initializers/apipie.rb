e7d311f40353b9350ed09d663bd12a0bf174c8ee
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2026-02-28 15:19:24 +0100

    fix: better docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,7 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "BlogBowl API"
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/apidoc"
-  # where is your API defined?
+  config.app_info                = "REST API for managing blog pages, posts, authors, newsletters, and subscribers."
   config.api_controllers_matcher = ["#{Rails.root}/submodules/core/app/controllers/api/v1/**/*.rb"]
   config.validate                = false
   config.generator.swagger.content_type_input = :json
